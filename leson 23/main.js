7ba86d8b5100e88e3bb55f2181094f38decfd4a6
Blini99 <blinhashani094@gmail.com>
2026-03-24 19:20:06 +0100

    w

---
+++ leson 23/main.js
@@ -0,0 +1,36 @@
+console.log("Hello World!");
+
+
+
+var p=4;
+var y=3
+var c=p*y
+console.log(c)
+
+var j=4;
+var k=3
+var c=j/k
+console.log(c)
+
+var a=2;
+var b=3;
+var c= c++
+console.log(c)
+
+
+var h=2;
+var n=3;
+var c= n--
+console.log(c)
+
+
+var n=99;
+n++;
+
+console.log(b1>b6);
+console.log(b1<b6);
+console.log(b1 =====6);
+var name="filan";
+var name="fisteku";
+
+console.log(name+surname)
