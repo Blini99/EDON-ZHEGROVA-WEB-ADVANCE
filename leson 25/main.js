9a49770aa1b50006effb74f5f8bbc38a679d1b45
Blini99 <blinhashani094@gmail.com>
2026-04-02 19:06:34 +0200

    ,

---
+++ leson 25/main.js
@@ -0,0 +1,50 @@
+var programmingLanguages = ['java','php','python'];
+
+
+console.log(programmingLanguages)
+
+
+console.log(programmingLanguages[3])
+
+programmingLanguages.push('js')
+console.log;
+
+programmingLanguages.pop()
+console.log(programmingLanguages);
+
+programmingLanguages.shift()
+console.log(programmingLanguages);
+
+// programmingLanguages.unshift('#c')
+// console.log(programmingLanguages);
+
+programmingLanguages.splice(0,2,'ruby');
+console.log(programmingLanguages);
+
+console.log(Math.random()*10);
+
+
+console.log(Math.floor(Math.random()*5));
+
+
+var students = ["john",'drin','filan'];
+
+
+var [s1,s2,s3] = students;
+
+
+console.log(s1);
+console.log(s2);
+console.log(s3);
+
+var places=["finlanda","prishtina","ferizaj","berlin"]
+var[firstplace,secondplace,thirdplace,fourthplace]=places;
+
+console.log(firstplace)
+console.log(secondplace)
+console.log(thirdplace)
+console.log(fourthplace)
+
+var=[1,2,3,4,5,6,7,8,9,10]
+var[firstnumber,secondnumber,.othernumber]=Number;
+
