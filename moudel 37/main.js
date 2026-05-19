cc2d434b8535b2593619005dde15f2f1837dabd6
Blini99 <blinhashani094@gmail.com>
2026-05-19 19:13:14 +0200

    n

---
+++ moudel 37/main.js
@@ -0,0 +1,24 @@
+$(function(){
+    alert("Hello");
+})
+
+$('#sort').sortable();
+
+$(function(){
+    $('#accordion').accordion();
+})
+
+$(function(){
+    $('#accordion').accordion({
+        collapsible:true
+    }
+
+    );
+})
+$('#tabs').tabs();
+
+$(function(){
+    $('#tabs').tabs({
+        collapsible:true;
+    });
+})
